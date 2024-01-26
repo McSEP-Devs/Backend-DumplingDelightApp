cd701ed8b97f27f56559dc4fa6bcdde15fbede77
Raj <rajkumarshahu@gmail.com>
2024-01-26 18:44:34 -0500

    Create user controller and route

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const {
+	getUsers,
+	getUser,
+	createUser,
+	updateUser,
+	deleteUser,
+} = require("../controllers/users");
+
+const User = require("../models/User");
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
+
+router.use(protect);
+//router.use(authorize('admin'));
+
+router
+	.route("/")
+	.get(advancedResults(User), getUsers)
+	.post(protect, authorize("admin"), createUser);
+
+router
+	.route("/:id")
+	.get(getUser)
+	.put(protect, authorize("admin", "customer"), updateUser)
+	.delete(protect, authorize("admin"), deleteUser);
+
+module.exports = router;
