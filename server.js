b1b47a10356e1c8c3473c82f48b90ffb80c7999a
Raj <rajkumarshahu@gmail.com>
2024-01-26 06:22:00 -0500

    Solve mongoose error

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require("express");
 const path = require("path");
 const colors = require("colors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+// @desc    Logs request to console
+const logger = (req, res, next) => {
+	console.log(
+		`${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+	);
+	next();
+};
+
+app.use(logger);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
