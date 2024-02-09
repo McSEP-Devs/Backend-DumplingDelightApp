80f7d17795011c6cc5bf472f25bfc6c143b4bd7f
Raj <rajkumarshahu@gmail.com>
2024-02-09 10:52:49 -0500

    Design Order Detail Model

---
+++ models/OrderDetail.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const OrderDetailSchema = new mongoose.Schema({
+	order: {
+		type: mongoose.Schema.ObjectId,
+		ref: "Order",
+		required: true,
+	},
+	item: {
+		type: mongoose.Schema.ObjectId,
+		ref: "Item",
+		required: false,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = mongoose.model("OrderDetail", OrderDetailSchema);
